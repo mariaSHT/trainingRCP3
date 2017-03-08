8a3928eebbcfa98740ed58317542340025eb353a
mladoue <mladoue@LFR019582.corp.capgemini.com>
2017-03-08 14:00:58 +0100

    Add colors

--- com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalAgencyView.java
+++ com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalAgencyView.java
@@ -1,39 +1,19 @@
 package com.sogeti.rental.ui;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EOperation;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
-import com.opcoach.training.rental.Address;
-import com.opcoach.training.rental.Customer;
-import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
-import com.opcoach.training.rental.RentalObject;
 import com.sogeti.rental.core.RentalActivator;
 
 public class RentalAgencyView extends ViewPart {
 	
 	private TreeViewer treeViewer;
 	
-	private static final Object[] EMPTY_RESULT = new Object[0];
+	public static final Object[] EMPTY_RESULT = new Object[0];
 
 	public RentalAgencyView() {
 		// TODO Auto-generated constructor stub
@@ -52,57 +32,12 @@ public class RentalAgencyView extends ViewPart {
 		agencies.add(RentalActivator.getAgency());
 		
 		treeViewer.setInput(agencies);
+		treeViewer.expandAll();
 	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
 
-	}
-
-	
-	private class RentalProvider extends LabelProvider implements ITreeContentProvider {
-
-		@Override
-		public Object[] getElements(Object inputElement) {
-			if (inputElement instanceof Collection<?>) {
-				return ((Collection) inputElement).toArray();
-			}
-			return EMPTY_RESULT;
-		}
-
-		@Override
-		public Object[] getChildren(Object parentElement) {
-			if (parentElement instanceof RentalAgency) {
-				return ((RentalAgency) parentElement).getCustomers().toArray();
-			}
-			return EMPTY_RESULT;
-		}
-
-		@Override
-		public Object getParent(Object element) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public boolean hasChildren(Object element) {
-			if (element instanceof RentalAgency) {
-				return true;
-			}
-			return false;
-		}
-		
-		
-		@Override
-		public String getText(Object element) {
-			if (element instanceof RentalAgency) {
-				return ((RentalAgency) element).getName();
-			}
-			else if (element instanceof Customer) {
-				return ((Customer) element).getDisplayName();
-			}
-			return super.getText(element);
-		}
 	}
 }
---
+++ com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalProvider.java
@@ -0,0 +1,141 @@
+package com.sogeti.rental.ui;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+
+import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalObject;
+
+class RentalProvider extends LabelProvider implements ITreeContentProvider, IColorProvider {
+
+	@Override
+	public Object[] getElements(Object inputElement) {
+		if (inputElement instanceof Collection<?>) {
+			return ((Collection) inputElement).toArray();
+		}
+		return RentalAgencyView.EMPTY_RESULT;
+	}
+
+	@Override
+	public Object[] getChildren(Object parentElement) {
+		if (parentElement instanceof RentalAgency) {
+			Collection<RentalNode> test = new ArrayList<>();
+			
+			test.add(new RentalNode(RentalNode.CUSTOMERS, (RentalAgency) parentElement));
+			test.add(new RentalNode(RentalNode.LOCATIONS, (RentalAgency) parentElement));
+			test.add(new RentalNode(RentalNode.OBJETCS, (RentalAgency) parentElement));	
+		
+			return test.toArray();
+		}
+		else if (parentElement instanceof RentalNode) {
+			return ((RentalNode) parentElement).getChildren();
+		}
+		
+		return null;
+	}
+
+	@Override
+	public Object getParent(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren(Object element) {
+		if (element instanceof RentalAgency) {
+			return true;
+		}
+		else if (element instanceof RentalNode) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	@Override
+	public String getText(Object element) {
+		if (element instanceof RentalAgency) {
+			return ((RentalAgency) element).getName();
+		}
+		else if (element instanceof Customer) {
+			return ((Customer) element).getDisplayName();
+		}
+		else if (element instanceof RentalObject) {
+			return ((RentalObject) element).getName();
+		}
+		else if (element instanceof RentalNode) {
+			return ((RentalNode) element).getLabel();
+		}
+		return super.getText(element);
+	}
+	
+	
+	 class RentalNode {
+		protected static final String CUSTOMERS = "Clients";
+		protected static final String LOCATIONS = "Locations";
+		protected static final String OBJETCS = "Objets à louer";
+		
+		private String label;
+		
+		private RentalAgency rentalAgency;
+		
+		public RentalNode(String label, RentalAgency rentalAgency) {
+			super();
+			this.label = label;
+			this.rentalAgency = rentalAgency;
+		}
+		
+		public Object[] getChildren() {
+			if (label == CUSTOMERS) {
+				return rentalAgency.getCustomers().toArray();
+			}
+			else if (label == LOCATIONS) {
+				return rentalAgency.getRentals().toArray();
+			}
+			else if (label == OBJETCS) {
+				return rentalAgency.getObjectsToRent().toArray();
+			}
+			
+			return null;
+		}
+		
+
+		public String getLabel() {
+			return label;
+		}
+
+		
+	}
+
+
+	@Override
+	public Color getForeground(Object element) {
+		if (element instanceof Customer) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_CYAN);
+		}
+		else if (element instanceof RentalObject) {
+			
+			return Display.getCurrent().getSystemColor(SWT.COLOR_GREEN);
+		}
+		else if (element instanceof RentalObject) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW);
+		}
+		
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
