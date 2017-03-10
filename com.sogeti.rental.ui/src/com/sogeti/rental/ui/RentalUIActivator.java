7a6d446b1fd11664972e30e5915f7fe1d38f9483
mladoue <mladoue@LFR019582.corp.capgemini.com>
2017-03-10 09:57:07 +0100

    List the view extension

--- com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalUIActivator.java
+++ com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalUIActivator.java
@@ -1,5 +1,8 @@
 package com.sogeti.rental.ui;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ColorRegistry;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
@@ -35,6 +38,19 @@ public class RentalUIActivator extends AbstractUIPlugin implements RentalUIConst
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		
+		readViewExt();
+	}
+
+	private void readViewExt() {
+		// Mettre un mecanisme pour charger
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		
+		for (IConfigurationElement e : reg.getConfigurationElementsFor("org.eclipse.ui.views")) {
+			if (e.getName().equals("view")) {
+				System.out.println("View: " + e.getAttribute("name"));
+			}
+		}
 	}
 
 	/*
